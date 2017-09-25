af845d14e0a90fc416e43871910dd7c7a1cad07d
dimitrioskyritsis <kiritsis.di@gmail.com>
2017-09-25 03:16:27 +0200

    fixing build issues, replacing with ng4

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,21 @@
+// shims needed for Angular
+import 'zone.js';
+import 'reflect-metadata'
+
 const angular = require('angular');
 import * as uiRouter from 'angular-ui-router';
 import { weatherModule } from './weather';
+import { AppModule } from './ng-upgrade'
+import { UpgradeModule } from '@angular/upgrade/static';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 angular.module('app', [
   uiRouter,
   weatherModule
 ]);
 
-import { upgradeAdapter } from './ng2-upgrade';
-document.addEventListener('DOMContentLoaded', () => {
-  upgradeAdapter.bootstrap(document.body, ['app']);
+
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.body, ['app']);
 });
---
+++ app/ng-upgrade.ts
@@ -0,0 +1,23 @@
+
+import 'zone.js';
+import 'reflect-metadata';
+import { NgModule } from '@angular/core';
+import { UpgradeModule } from '@angular/upgrade/static';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { WeatherPreview } from './weather/weather-preview'
+import { WeatherService } from './weather/weather.service'
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        UpgradeModule,
+        HttpModule,
+    ],
+    declarations: [ WeatherPreview ],
+    entryComponents: [ WeatherPreview ],
+    providers: [ WeatherService ],
+})
+export class AppModule {
+    ngDoBootstrap() {}
+}
